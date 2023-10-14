8b1312bf5797ea33ffb22da61a5f483a2b83e0e8
JavierP <javier.poblete2023@alu.uct.cl>
2023-10-14 00:49:10 -0300

    localhost

--- Frotend/Pages/PHP/conexion_2.php
+++ Frotend/Pages/PHP/conexion_2.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conexion = mysqli_connect('localhost', 'root', '', 'login');
+    $conexion = mysqli_connect("db.inf.uct.cl", "jpoblete", "21736669", "A2023_jpoblete");
 
     if($conexion){
         echo "funciona";
