bb00c808eb69086445e1a0d90403f05ab99dcadd
JavierP <javier.poblete2023@alu.uct.cl>
2023-10-16 01:09:33 -0300

    inicio de sesion mejorado

--- Frotend/Pages/Inicio_sesion.php
+++ Frotend/Pages/Inicio_sesion.php
@@ -1,33 +1,31 @@
 <?php
 
-$conn = new mysqli('localhost', 'root', '', 'login');
-$response = "";
-if ($conn->connect_error) {
-    $response = array('error' => 'Error de conexión: ' . $conn->connect_error);
-} else {
-    if (isset($_POST["subir"])) { 
-        if(isset($_POST['rut']) && isset($_POST['email'])){
-            $rut = $_POST['rut'];
-            $correo = $_POST['email'];
-            $sql = "SELECT * FROM usuarios WHERE rut = $rut AND correo = '$correo'";
-            $result = $conn->query($sql);
-            
-            if ($result->num_rows > 0) {
-            
-                while ($row=mysqli_fetch_row($result))
-                {
-                    if($nombre == $row[0] && $row[3] == $contra){
-                        session_start();
-                        $_SESSION['email'] = $nombre;
-                }
-                header('Location:products.html');
-                
-            }
-        }     
-    }
-    }
+if(isset($_POST["subir"])){
+    include("conexion_2.php");
 
+    $rut=($_POST['rut']);
+    $correo=($_POST['email']);
+
+    $sql=$conn->prepare("SELECT * FROM usuarios WHERE rut=:rut AND correo=:correo");
+
+    $sql->bindParam("rut",$rut, PDO::PARAM_INT);
+    $sql->bindParam("correo",$correo, PDO::PARAM_STR);
+    $sql->execute();
+
+    $registro=$sql->fetch(PDO::FETCH_ASSOC);
+
+    print_r($registro);
+
+    session_start();
+    $_SESSION['correo']=$registro;
+
+    $numeroregs = $sql->rowCount();
+
+    if($numeroregs>=1){
+        echo "Bienvenido....";
+        header('Location:products.html');
+    }else{
+
+    }
 }
-header('Content-Type: application/json');
-echo json_encode($response);
 ?>
--- Frotend/Pages/conexion_2.php
+++ Frotend/Pages/conexion_2.php
@@ -1,5 +1,17 @@
 <?php
 
-$conexion= new mysqli("localhost", "root", "", "login");
-$conexion->set_charset("utf8");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+
+try {
+
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "<script> alert('Conexion exitosa'); </script>";
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 ?>
