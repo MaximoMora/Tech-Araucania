9fbe86821a25102855859da8687205d0ad2c47a2
Maximo Mora <maxmora401@gmail.com>
2023-10-24 17:20:23 -0300

    visual me pidio cambiar el registro

--- Frotend/Pages/registro.php
+++ Frotend/Pages/registro.php
@@ -1,16 +1,9 @@
 <?php
-<<<<<<< HEAD
-session_start(); // Iniciar la sesión
-
-$conn = new mysqli('db.inf.uct.cl', 'jpoblete', '21736669', 'A2023_jpoblete');
-$response = array();
-=======
 $conn = new mysqli('db.inf.uct.cl', 'jpoblete', '21736669', 'A2023_jpoblete');
 $response = "";
->>>>>>> 267243af888671b2c01b5d5b20780e79f16d4393
 
 if ($conn->connect_error) {
-    $response = array('error' => 'Error de conexión: ' . utf8_encode($conn->connect_error));
+    $response = array('error' => 'Error de conexiï¿½n: ' . utf8_encode($conn->connect_error));
 } else {
     if (isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["direccion"])) {
         $nombre = ($_POST["nombre"]);
